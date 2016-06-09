8b70ff24ff28678ff2df063e850c4fdbd1b4c882
Patric keisala <finne_no1@hotmail.com>
2016-06-09 09:34:57 +0200

    json

--- studieplaner/data.js
+++ studieplaner/data.js
@@ -1,3 +1,93 @@
+var mobileWebDev = [
+        {
+            title: "Teambuilding", 
+            startDate: "2015-08-24", 
+            endDate: "2015-08-28"
+        },
+        {
+            title: "Branschkunskap för webbutvecklare", 
+            startDate: "2015-08-31", 
+            endDate: "2015-09-11"
+        },
+        {
+            title: "Gränssnitt och grafik", 
+            startDate: "2015-09-14", 
+            endDate: "2015-10-02"
+        },
+        {
+            title: "HTML & CSS", 
+            startDate: "2015-10-05 ", 
+            endDate: "2015-10-16"
+        },
+        {
+            title: "LIA preparand", 
+            startDate: "2015-10-19", 
+            endDate: "2015-10-23"
+        },
+        {
+            title: "Responsiv Webb Design 1", 
+            startDate: "2015-10-26", 
+            endDate: "2015-11-27"
+        },
+        {
+            title: "User experience design UX", 
+            startDate: "2015-11-30", 
+            endDate: "2015-12-18"
+        },
+        {
+            title: "Javascript och programmeringsmetodik", 
+            startDate: "2016-01-11", 
+            endDate: "2016-04-01"
+        },
+        {
+            title: "LIA1", 
+            startDate: "2016-04-04", 
+            endDate: "2016-06-03"
+        },
+        {
+            title: "Utvecklingsmetodik och agila metoder ", 
+            startDate: "2016-06-06", 
+            endDate: "2016-06-24"
+        },
+        {
+            title: "Responsiv Webb Design 2", 
+            startDate: "2016-08-22",
+             endDate: "2016-09-23"
+            },
+        {
+            title: "PHP, SQL och servertjänster", 
+            startDate: "2016-09-26", 
+            endDate: "2016-11-18"
+        },
+        {
+            title: "CMS och integration", 
+            startDate: "2016-11-21", 
+            endDate: "2016-12-16"
+        },
+        {
+            title: "Hybridapplikationer", 
+            startDate: "2017-01-09", 
+            endDate: "2017-02-03"
+        },
+        {
+            title: "LIA2", 
+            startDate: "2017-02-06", 
+            endDate: "2017-04-14"
+        },
+        {
+            title: "Examensarbete", 
+            startDate: "2017-04-17", 
+            endDate: "2017-06-09"
+        }];
+        
+var frontWebDev = [
+        {}
+
+
+]
+
+
+
 [
     { 
     "name": "Frontend Web Developer (tidigare Mobile Web Developer)",
