ea9067847fb0b33d2ba7af71f78df709acbb05c0
Rasmus <hallstrom.rasmus@gmail.com>
2016-06-09 10:47:51 +0200

    Commit

--- projekt/desktop-app/gulpfile.js
+++ projekt/desktop-app/gulpfile.js
@@ -1,17 +1,23 @@
 'use strict';
 
-var gulp        = require('gulp');
-var browserSync = require('browser-sync').create();
-var sass        = require('gulp-sass');
+var gulp        = require('gulp'),
+    browserSync = require('browser-sync').create(),
+    sass        = require('gulp-sass'),
+    gutil       = require('gulp-util'),
+    browserify  = require('gulp-browserify'),
+    concat      = require('gulp-concat'),
+    clean       = require('gulp-clean');
+
 
 // Static Server + watching scss/html files
 gulp.task('serve', ['sass'], function() {
 
     browserSync.init({
-        server: "./src/"
+        server: "./"
     });
 
     gulp.watch("src/assets/sass/*.scss", ['sass']);
+    gulp.watch('src/app/**/*.js',['browserify']).on('change', browserSync.reload);
     gulp.watch("src/*.html").on('change', browserSync.reload);
 });
 
@@ -21,6 +27,20 @@ gulp.task('sass', function() {
         .pipe(sass())
         .pipe(gulp.dest("build/assets/css"))
         .pipe(browserSync.stream());
+});
+
+// Browserify task
+gulp.task('browserify', function() {
+  // Single point of entry (make sure not to src ALL your files, browserify will figure it out for you)
+  gulp.src(['src/app/main.js'])
+  .pipe(browserify({
+    insertGlobals: true,
+    debug: true
+  }))
+  // Bundle to a single file
+  .pipe(concat('bundle.js'))
+  // Output it to our dist folder
+  .pipe(gulp.dest('build/js'));
 });
 
 gulp.task('default', ['serve']);
