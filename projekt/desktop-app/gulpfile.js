f40a5527d1de6493f0a7061120f2cbaf30a62450
Rasmus <hallstrom.rasmus@gmail.com>
2016-06-09 11:10:40 +0200

    gulp is the shit

--- projekt/desktop-app/gulpfile.js
+++ projekt/desktop-app/gulpfile.js
@@ -3,10 +3,6 @@
 var gulp        = require('gulp'),
     browserSync = require('browser-sync').create(),
     sass        = require('gulp-sass'),
-    gutil       = require('gulp-util'),
-    browserify  = require('gulp-browserify'),
-    concat      = require('gulp-concat'),
-    clean       = require('gulp-clean');
 
 
 // Static Server + watching scss/html files
@@ -16,9 +12,9 @@ gulp.task('serve', ['sass'], function() {
       server: "./"
     });
 
-    gulp.watch("src/assets/sass/*.scss", ['sass']);
-    gulp.watch('src/app/**/*.js',['browserify']).on('change', browserSync.reload);
-    gulp.watch("src/*.html").on('change', browserSync.reload);
+    gulp.watch("assets/sass/*.scss", ['sass']);
+    gulp.watch('app/**/*.js',['browserify']).on('change', browserSync.reload);
+    gulp.watch("*.html").on('change', browserSync.reload);
 });
 
 // Compile sass into CSS & auto-inject into browsers
@@ -27,20 +23,6 @@ gulp.task('sass', function() {
         .pipe(sass())
         .pipe(gulp.dest("build/assets/css"))
         .pipe(browserSync.stream());
-});
-
-// Browserify task
-gulp.task('browserify', function() {
-  // Single point of entry (make sure not to src ALL your files, browserify will figure it out for you)
-  gulp.src(['src/app/main.js'])
-  .pipe(browserify({
-    insertGlobals: true,
-    debug: true
-  }))
-  // Bundle to a single file
-  .pipe(concat('bundle.js'))
-  // Output it to our dist folder
-  .pipe(gulp.dest('build/js'));
 });
 
 gulp.task('default', ['serve']);
