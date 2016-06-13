9fc3d95578aa9942c124062cd52f029dfd16963e
fransbernhard <mimilundberg@icloud.com>
2016-06-13 14:15:45 +0200

    intendent properly

--- projekt/desktop-app/app/components/student/student.js
+++ projekt/desktop-app/app/components/student/student.js
@@ -1,31 +1,29 @@
 app.controller('student', ['$scope', '$state', '$stateParams', '$calendar', 'uiCalendarConfig', function($scope, $state, $stateParams, $calendar, uiCalendarConfig) {
 var date = new Date();
-    var d = date.getDate();
-    var m = date.getMonth();
-    var y = date.getFullYear();
-
-
-
+var d = date.getDate();
+var m = date.getMonth();
+var y = date.getFullYear();
 
 $scope.events = [];
 
 $calendar.getSchedule('MWD').then(function (result) {
-    $scope.course = result.data;
-    angular.forEach(result.data.content, function(val, index) {
-        $scope.events.push(val);
-        console.log($scope.events)
-    });
+  $scope.course = result.data;
+  angular.forEach(result.data.content, function(val, index) {
+    $scope.events.push(val);
+    console.log($scope.events);
+  });
 });
+
 $scope.changeView = function(view,calendar) {
-      uiCalendarConfig.calendars[calendar].fullCalendar('changeView',view);
+  uiCalendarConfig.calendars[calendar].fullCalendar('changeView',view);
 };
 
- $scope.eventsF = function (start, end, timezone, callback) {
-      var s = new Date(start).getTime() / 1000;
-      var e = new Date(end).getTime() / 1000;
-      var m = new Date(start).getMonth();
-      callback($scope.events);
-    };
+$scope.eventsF = function (start, end, timezone, callback) {
+  var s = new Date(start).getTime() / 1000;
+  var e = new Date(end).getTime() / 1000;
+  var m = new Date(start).getMonth();
+  callback($scope.events);
+};
 
 $scope.eventSources = [$scope.events, $scope.eventsF];
 
@@ -36,7 +34,7 @@ $calendar.getSchedule()
     angular.forEach(data, function(val, key) {
       $scope.courses.push({name: key, content: val});
     });
+    console.log($scope.courses);
   });
-
 
 }]);
