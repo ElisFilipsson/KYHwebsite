8a154075f29e4e5c7c05db2e70469236ef9226b5
Patric keisala <finne_no1@hotmail.com>
2016-06-13 13:29:27 +0200

    vet ej

--- projekt/desktop-app/app/components/student/student.js
+++ projekt/desktop-app/app/components/student/student.js
@@ -13,13 +13,21 @@ $calendar.getSchedule('MWD').then(function (result) {
     $scope.course = result.data;
     angular.forEach(result.data.content, function(val, index) {
         $scope.events.push(val);
+        console.log($scope.events)
     });
 });
 $scope.changeView = function(view,calendar) {
       uiCalendarConfig.calendars[calendar].fullCalendar('changeView',view);
 };
 
-$scope.eventSources = [$scope.events];
+ $scope.eventsF = function (start, end, timezone, callback) {
+      var s = new Date(start).getTime() / 1000;
+      var e = new Date(end).getTime() / 1000;
+      var m = new Date(start).getMonth();
+      callback($scope.events);
+    };
+
+$scope.eventSources = [$scope.events, $scope.eventsF];
 
 
 
