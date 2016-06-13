aaf0efac6f397fc277a0e7d7f9eb05d8505dbd8e
Sandra Filipsson <ardnassandra13@gmail.com>
2016-06-13 10:22:57 +0200

    start on contact/vistor

---
+++ projekt/desktop-app/app/components/visitor/visitor.js
@@ -0,0 +1,10 @@
+app.controller('ExampleController', ['$scope', function($scope) {
+      $scope.list = [];
+      $scope.text = 'hello';
+      $scope.submit = function() {
+        if ($scope.text) {
+          $scope.list.push(this.text);
+          $scope.text = '';
+        }
+     };
+}]);
