e6457cb00565d36a2403ae1c0c400f0f2bf97f22
Alexander D'Este <alexanderdeste@gmail.com>
2016-06-14 14:31:48 +0200

    email controller and factory

--- projekt/desktop-app/app/components/visitor/visitor.js
+++ projekt/desktop-app/app/components/visitor/visitor.js
@@ -1,8 +1,8 @@
-app.controller('visitorCtrl', ['$scope', '$calendar', 'toaster', function($scope, $calendar, toaster) {
+app.controller('visitorCtrl', ['$scope', '$calendar', 'toaster', 'email', function($scope, $calendar, toaster, email) {
     $scope.list = [];
     $scope.text = 'Förnamn Efternamn';
     $scope.email = 'email@adress.se';
-    $scope.showMessage = false; 
+    $scope.showMessage = false;
     $scope.emailMessage = '';
     $scope.education = 'MWD';
 
@@ -14,11 +14,11 @@ app.controller('visitorCtrl', ['$scope', '$calendar', 'toaster', function($scope
       // adding toaster-alert on error
       if(regMail.test($scope.email) === false) {
         toaster.error('Email', 'Du har inte skrivit in en giltig emailadress!');
-      } 
-      if (text.test($scope.text) === false) { 
+      }
+      if (text.test($scope.text) === false) {
         toaster.error('Namn', 'Du har inte skrivit in ett giltigt namn!');
-      } 
-      else if ($scope.education === '') { 
+      }
+      else if ($scope.education === '') {
         toaster.info('Utbildning', 'Du måste välja en utbildning du vill ha information om.');
       }  else {
         // adding toaster-alert on success
@@ -27,24 +27,30 @@ app.controller('visitorCtrl', ['$scope', '$calendar', 'toaster', function($scope
       }
       return temp;
     };
-    
+
     $scope.submit = function() {
       if($scope.validateForm()) {
         // function for sending email here please!
         // setup e-mail data with unicode symbols
         var mailOptions = {
-            from: '"Fred Foo 👥" <foo@blurdybloop.com>', // sender address
-            to: 'bar@blurdybloop.com, baz@blurdybloop.com', // list of receivers
+            from: '"Fred Man Guy 👥" <foo@blurdybloop.com>', // sender address
+            to: 'finne_no1@hotmail.com, alexanderdeste+spam@gmail.com, ardnassandra13@gmail.com', // list of receivers
             subject: 'Hello ✔', // Subject line
             text: 'Hello world 🐴', // plaintext body
-            html: '<b>Hello world 🐴</b>' // html body
+            html: '<b>Hello world, 🐴</b>' // html body
         };
+
+        email
+          .sendEmail(mailOptions)
+          .then(function (result) {
+            console.log(result);
+          });
       }
     };
-    
+
     $calendar.getSchedule().then(function (result) {
         $scope.utbildningslista = result.data;
         console.log(result.data);
     });
-    
+
 }]);
