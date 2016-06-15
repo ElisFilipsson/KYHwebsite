22c3b304841f485ec2f3b82d717605277cc7a800
Alexander D'Este <alexanderdeste@gmail.com>
2016-06-15 14:35:04 +0200

    Calendar language changed to swedsih, style applied to list

--- projekt/desktop-app/app/components/student/student.js
+++ projekt/desktop-app/app/components/student/student.js
@@ -1,54 +1,54 @@
 app.controller('student', ['$scope', '$state', '$stateParams', '$calendar', 'uiCalendarConfig', function($scope, $state, $stateParams, $calendar, uiCalendarConfig) {
 
-var id = $stateParams.id;
+    var id = $stateParams.id;
 
-var date = new Date(),
-  d = date.getDate(),
-  m = date.getMonth(),
-  y = date.getFullYear();
+    var date = new Date(),
+        d = date.getDate(),
+        m = date.getMonth(),
+        y = date.getFullYear();
 
-$scope.events = [];
-$scope.tempevents = [];
-$scope.firstCourse = '';
-$scope.selectStartDate = '2016-06-06';
+    $scope.events = [];
+    $scope.tempevents = [];
+    $scope.firstCourse = '';
+    $scope.selectStartDate = '2016-06-06';
 
-$calendar.getSchedule(id).then(function (result) {
-  $scope.course = result.data;
-  angular.forEach(result.data.content, function(val, index) {
-    $scope.events.push(val);
-    $scope.tempevents.push(val);
-  });
-});
+    $calendar.getSchedule(id).then(function(result) {
+        $scope.course = result.data;
+        angular.forEach(result.data.content, function(val, index) {
+            $scope.events.push(val);
+            $scope.tempevents.push(val);
+        });
+    });
 
-$scope.changeView = function(view,calendar) {
-  $('#calendar').fullCalendar('changeView',view);
-};
+    $scope.changeView = function(view, calendar) {
+        $('#calendar').fullCalendar('changeView', view);
+    };
 
-$scope.eventsF = function (start, end, timezone, callback) {
-  var s = new Date(start).getTime() / 1000;
-  var e = new Date(end).getTime() / 1000;
-  var m = new Date(start).getMonth();
-  callback($scope.events);
-  colorizeCalendar();
-};
+    $scope.eventsF = function(start, end, timezone, callback) {
+        var s = new Date(start).getTime() / 1000;
+        var e = new Date(end).getTime() / 1000;
+        var m = new Date(start).getMonth();
+        callback($scope.events);
+        colorizeCalendar();
+    };
 
 
     $scope.getNextCourseDate = function() {
-        $calendar.getSchedule(id).then(function (result) {
-           var date = '';
-           angular.forEach(result.data.content, function(val, index){
-              if (date === '') {
-                if ( moment().format('YYYY-MM-DD') <= val.start ){
-                   date = val.start;
-                   $scope.selectStartDate = val.start;
+        $calendar.getSchedule(id).then(function(result) {
+            var date = '';
+            angular.forEach(result.data.content, function(val, index) {
+                if (date === '') {
+                    if (moment().format('YYYY-MM-DD') <= val.start) {
+                        date = val.start;
+                        $scope.selectStartDate = val.start;
+                    }
                 }
-              }
-           });
+            });
 
-           $scope.events = [];
-           $scope.events = angular.copy($scope.tempevents);
-           $('#calendar').fullCalendar('gotoDate', date); 
-           
+            $scope.events = [];
+            $scope.events = angular.copy($scope.tempevents);
+            $('#calendar').fullCalendar('gotoDate', date);
+
 
         });
     };
@@ -56,43 +56,54 @@ $scope.eventsF = function (start, end, timezone, callback) {
     $scope.getNextCourseDate();
     $scope.uiConfig = {
         calendar: {
-          defaultView: "month",
-          editable: true,
-          header: {
-            left: 'title',
-            center: '',
-            right: 'today prev,next',
-          },
-          dayClick: $scope.goToRootScopeDate,
-          defaultDate: $scope.selectStartDate
+            lang: 'sv',
+            defaultView: "month",
+            editable: true,
+            header: {
+                left: 'title',
+                center: '',
+                right: 'today prev,next',
+            },
+            dayClick: $scope.goToRootScopeDate,
+            defaultDate: $scope.selectStartDate
         },
-      };
+    };
 
 
-$scope.eventSources = [$scope.eventsF];
-
-
-
+    $scope.eventSources = [$scope.eventsF];
 
 
 
 
-$scope.classes = [];
-$calendar.getSchedule(id)
-  .then(function(res) {
-    var data = res.data;
-    angular.forEach(data.content, function(val, key) {
-      $scope.classes.push({id: key, title: val.title});
-    });
-  });
 
 
-$scope.courses = [];
-$calendar.getSchedule()
-   .then(function(res) {
-     var data = res.data;
-     angular.forEach(data, function(val, key) {
-       $scope.courses.push({name: key, content: val});
-     });
-   });
+
+    $scope.classes = [];
+    $calendar.getSchedule(id)
+        .then(function(res) {
+            var data = res.data;
+            angular.forEach(data.content, function(val, key) {
+                $scope.classes.push({
+                    id: key,
+                    title: val.title
+                });
+            });
+        });
+
+
+    $scope.courses = [];
+    $calendar.getSchedule()
+        .then(function(res) {
+            var data = res.data;
+            angular.forEach(data, function(val, key) {
+                $scope.courses.push({
+                    name: key,
+                    content: val
+                });
+            });
+        });
+
+    $scope.stringToColor = stringToColor;
+    $scope.getReadableColor = getReadableColor;
+
 }]);
